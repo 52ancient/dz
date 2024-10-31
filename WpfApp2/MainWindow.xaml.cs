8583ef23334fd76884ead69fd10779d75a293c79
52ancient <46.fm.rnb@gmail.com>
2024-10-31 10:40:16 +0400

    Blue

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            MessageBox.Show("Привет!");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Close();
+        
+        }
+
+        private void _123_Loaded(object sender, RoutedEventArgs e)
+        {
+            _123.Background = Brushes.Blue;
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Зеленый!");
+        }
+
+        private void Button_Loaded(object sender, RoutedEventArgs e)
+        {
+            _33.Background = Brushes.BlueViolet;
+        }
+    }
+}
